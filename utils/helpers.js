7f7cad44b7d5060668658b4e707bcfc3576702e4
Wes <wesleySalesberry@hotmail.com>
2020-12-20 17:41:41 -0500

    stores multer and cloudinary

---
+++ utils/helpers.js
@@ -0,0 +1,25 @@
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+	destination: function (req, res, callback){
+		callback(null, './uploads/')
+	},
+	filename: function(req, file, callback){
+		callback(null, new Date().toISOString() + '_' + file.originalname)
+	}
+})
+
+const fileFilter = (req, file, callback)=> {
+	if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' ||file.mimetype === 'image/jpg'){
+		callback(null, true)
+	}else{
+		callback({message: "Unsupported file format"}, false)
+	}
+}
+
+const upload = multer({
+	storage: storage,
+	fileFilter: fileFilter
+})
+
+module.exports = upload
